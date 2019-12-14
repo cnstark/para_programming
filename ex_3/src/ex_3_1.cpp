6c5c4acbb6dbb57b823f4a0df6900c5b649336bc
pp11 <pp11@email.ustc.edu.cn>
2019-12-14 12:56:18 +0800

    gather result to rank 0

--- ex_3/src/ex_3_1.cpp
+++ ex_3/src/ex_3_1.cpp
@@ -19,6 +19,11 @@
         std::cout << std::endl; \
     }
 
+#define FREE(x) if (x != nullptr) {\
+        free(x); \
+        x = nullptr; \
+    }
+
 #define FILE_NAME "1.txt"
 #define NODE_NUM 50
 
@@ -47,6 +52,7 @@ void mpi_dijkstra(MPIArg arg) {
     int n, s;
     double *matrix;
     double *part;
+    // 读取矩阵并广播n和s
     if (arg.rank == 0) {
         s = NODE_NUM;
         FILE *fp = fopen(FILE_NAME, "r");
@@ -74,35 +80,27 @@ void mpi_dijkstra(MPIArg arg) {
         MPI_Bcast(&s, 1, MPI_INT, 0, arg.comm);
     }
 
+    // 计算每个处理器负责的行数
     int ep = (int)ceil(n / (double)arg.rank_size);
     int mynum = ep;
     if (arg.rank == arg.rank_size - 1) {
         mynum = n - ep * (arg.rank_size - 1);
     }
-    // std::cout << "Rank: " << arg.rank << ", ep: " << ep << std::endl;
-    // std::cout << "Rank: " << arg.rank << ", mynum: " << mynum << std::endl;
-    // std::cout << "Rank: " << arg.rank << ", n: " << n << std::endl;
-    // std::cout << "Rank: " << arg.rank << ", s: " << s << std::endl;
+
+    // 分配矩阵
     part = (double *)malloc(sizeof(double) * ep * n);
     if (arg.rank == 0) {
         MPI_Scatter(matrix, ep * n, MPI_DOUBLE, part, ep * n, MPI_DOUBLE, 0, arg.comm);
-        free(matrix);
+        FREE(matrix)
     } else {
         MPI_Scatter(part, ep * n, MPI_DOUBLE, part, ep * n, MPI_DOUBLE, 0, arg.comm);
     }
 
-    // for(int i = 0; i < mynum; i++) {
-    //     std::cout << "Rank " << arg.rank << ":\t";
-    //     for(int j = 0; j < n; j++) {
-    //         std::cout << VALUE(part, n, i, j) << "\t";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
     MPI_Status status;
+
+    // 初始化dist和bdist
     double *dist = (double *)malloc(sizeof(double) * ep);
     bool *bdist = (bool *)malloc(sizeof(bool) * ep);
-
     for(int i = 0; i < ep; i++) {
         if(i + arg.rank * ep == s) {
             dist[i] = 0;
@@ -113,7 +111,7 @@ void mpi_dijkstra(MPIArg arg) {
         }
     }
 
-
+    // 算法主循环
     for (int i = 0; i < n; i++) {
         int index = 0, index2;
         double num = 99999, num2;
@@ -125,44 +123,11 @@ void mpi_dijkstra(MPIArg arg) {
                 index = ep * arg.rank + j;
             }
         }
-        
-        // std::cout << "Rank " << arg.rank << " debug" << std::endl;
 
         MPI_Barrier(arg.comm);
-        // if (arg.rank == 1)
-        // std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
 
         /**步骤(3.2)**/
         int calnum = arg.rank_size;
-
-        // while (calnum > 1) {
-        //     if (calnum % 2 == 0) {
-        //         calnum /= 2;
-        //         if (arg.rank < calnum) {
-        //             MPI_Recv(&index2, 1, MPI_INT, arg.rank + calnum, arg.rank, arg.comm, &status);
-        //             MPI_Recv(&num2, 1, MPI_DOUBLE, arg.rank + calnum, arg.rank, arg.comm, &status);
-        //             if(num2 < num) {
-        //                 num = num2;
-        //                 index = index2;
-        //             }
-        //         } else {
-        //             MPI_Send(&index, 1, MPI_INT, arg.rank - calnum, arg.rank - calnum, arg.comm);
-        //             MPI_Send(&num, 1, MPI_DOUBLE, arg.rank - calnum, arg.rank - calnum, arg.comm);
-        //         }
-        //     } else {
-        //         if (arg.rank < calnum - 1) {
-        //             MPI_Recv(&index2, 1, MPI_INT, arg.rank + calnum, arg.rank, arg.comm, &status);
-        //             MPI_Recv(&num2, 1, MPI_DOUBLE, arg.rank + calnum, arg.rank, arg.comm, &status);
-        //             if(num2 < num) {
-        //                 num = num2;
-        //                 index = index2;
-        //             }
-        //         } else if ()
-        //     }
-        // }
-
-
-
         while (calnum > 1) {
             if (calnum % 2 == 0) {
                 if (arg.rank < calnum) {
@@ -204,14 +169,9 @@ void mpi_dijkstra(MPIArg arg) {
             MPI_Barrier(arg.comm);
         }
 
-
-        // if (arg.rank == 1)
-        // std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
-
         /**步骤(3.3)**/
         MPI_Bcast(&index, 1, MPI_INT, 0, arg.comm);
         MPI_Bcast(&num, 1, MPI_DOUBLE, 0, arg.comm);
-        // std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
 
         /**步骤(3.4)**/
         for(int j = 0; j < mynum; j++) {
@@ -226,128 +186,21 @@ void mpi_dijkstra(MPIArg arg) {
 
         MPI_Barrier(arg.comm);
     }
-    for(int i = 0; i < mynum; i++) {
-        std::cout << "node  " << arg.rank * ep + i << ":\t" << (int)dist[i] << std::endl;
+
+    if (arg.rank == 0) {
+        double *all_dist = (double *)malloc(sizeof(double) * (ep * arg.rank_size));
+        MPI_Gather(dist, ep, MPI_DOUBLE, all_dist, ep, MPI_DOUBLE, 0, arg.comm);
+        for(int i = 0; i < n; i++) {
+            std::cout << "node  " << i << ":\t" << (int)all_dist[i] << std::endl;
+        }
+        FREE(all_dist)
+    } else {
+        MPI_Gather(dist, ep, MPI_DOUBLE, dist, ep, MPI_DOUBLE, 0, arg.comm);
     }
 
-    // if (arg.rank == 0) {
-    //     // MPI_Comm new_comm1;
-    //     // MPI_Comm_split(arg.comm, 0, 0, &new_comm1);/
-    //     for (int i = 1; i < arg.rank_size; i++) {
-    //         MPI_Send(matrix + ep * (i - 1), ep * n, MPI_DOUBLE, i, i, arg.comm);
-    //     }
-    //     for (int i = 0; i < n; i++) {
-    //         int index;
-    //         double num;
-    //         MPI_Bcast(&index, 1, MPI_INT, 1, arg.comm);
-	// 	    MPI_Bcast(&num, 1, MPI_DOUBLE, 1, arg.comm);
-    //         // std::cout << "Rank: " << arg.rank << ", index: " << index << std::endl;
-    //         // std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
-    //         MPI_Barrier(arg.comm);
-    //     }
-    // } else {
-    //     MPI_Status status;
-    //     double *dist = (double *)malloc(sizeof(double) * ep);
-	// 	bool *bdist = (bool *)malloc(sizeof(bool) * ep);
-    //     matrix = (double *)malloc(sizeof(double) * ep * n);
-
-    //     // MPI_Comm new_comm;
-    //     // MPI_Comm_split(arg.comm, 1, arg.rank - 1, &new_comm);
-
-    //     MPI_Recv(matrix, ep * n, MPI_DOUBLE, 0, arg.rank, arg.comm, &status);
-        
-    //     for(int i = 0; i < ep; i++) {
-	// 		if(i + (arg.rank - 1) * ep == s) {
-	// 			dist[i] = 0;
-	// 			bdist[i] = true;
-	// 		} else {
-	// 			dist[i] = VALUE(matrix, n, i, s);
-	// 			bdist[i] = false;
-	// 		}
-	// 	}
-
-    //     // for(int i = 0; i < mynum; i++) {
-	// 	// 	std::cout << "Rank " << arg.rank << ":\t";
-	// 	// 	for(int j = 0; j < n; j++) {
-    //     //         std::cout << VALUE(matrix, n, i, j) << "\t";
-	// 	// 	}
-	// 	// 	std::cout << std::endl;
-	// 	// }
-    //     for (int i = 0; i < n; i++) {
-    //         int index = 0, index2;
-    //         double num = 99999, num2;
-
-    //         /**步骤(3.1)**/
-    //         for (int j = 0; j < mynum; j++) {
-    //             if (dist[j] < num && bdist[j] == false) {
-    //                 num = dist[j];
-    //                 index = ep * (arg.rank - 1) + j;
-    //             }
-    //         }
-            
-    //         // std::cout << "Rank " << arg.rank << " debug" << std::endl;
-
-    //         // MPI_Barrier(new_comm);
-    //         if (arg.rank == 1)
-    //         std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
-
-    //         /**步骤(3.2)**/
-    //         int calnum = arg.rank_size - 1;
-    //         while (calnum > 1) {
-    //             if (calnum % 2 == 0) {
-    //                 calnum = calnum / 2;
-    //                 if(arg.rank > calnum) {
-    //                     MPI_Send(&index, 1, MPI_INT, arg.rank - calnum, arg.rank - calnum, arg.comm);
-    //                     MPI_Send(&num, 1, MPI_DOUBLE, arg.rank - calnum, arg.rank - calnum, arg.comm);
-    //                 } else {
-    //                     MPI_Recv(&index2, 1, MPI_INT, arg.rank + calnum, arg.rank, arg.comm, &status);
-    //                     MPI_Recv(&num2, 1, MPI_DOUBLE, arg.rank + calnum, arg.rank, arg.comm, &status);
-    //                     if(num2 < num) {
-    //                         num = num2;
-    //                         index = index2;
-    //                     }
-    //                 }
-    //             } else {
-    //                 calnum = (calnum + 1) / 2;
-    //                 if(arg.rank > calnum) {
-    //                     MPI_Send(&index, 1, MPI_INT, arg.rank - calnum, arg.rank - calnum, arg.comm);
-    //                     MPI_Send(&num, 1, MPI_DOUBLE, arg.rank - calnum, arg.rank - calnum, arg.comm);
-    //                 } else if(arg.rank < calnum) {
-    //                     MPI_Recv(&index2, 1, MPI_INT, arg.rank + calnum, arg.rank, arg.comm, &status);
-    //                     MPI_Recv(&num2, 1, MPI_DOUBLE, arg.rank + calnum, arg.rank, arg.comm, &status);
-    //                     if(num2 < num) {
-    //                         num = num2;
-    //                         index = index2;
-    //                     }
-    //                 }
-    //             }
-    //             // MPI_Barrier(new_comm);
-    //         }
-    //         // if (arg.rank == 1)
-    //         // std::cout << "Rank: " << arg.rank << ", num: " << num << std::endl;
-
-    //         /**步骤(3.3)**/
-    //         MPI_Bcast(&index, 1, MPI_INT, 1, arg.comm);
-	// 	    MPI_Bcast(&num, 1, MPI_DOUBLE, 1, arg.comm);
-
-    //         /**步骤(3.4)**/
-    //         for(int j = 0; j < mynum; j++) {
-    //             if((bdist[j] == false) && (num + VALUE(matrix, n, j, index) < dist[j]))
-    //                 dist[j] = num + VALUE(matrix, n, j, index);
-    //         }
-
-    //         /**步骤(3.5)**/
-    //         if(arg.rank == index / ep + 1) {
-    //             bdist[index % ep] = true;
-    //         }
-
-    //         MPI_Barrier(arg.comm);
-    //     }
-
-    //     for(int i = 0; i < mynum; i++) {
-    //         std::cout << "node  " << (arg.rank - 1) * ep + i << ":\t" << (int)dist[i] << std::endl;
-    //     }
-    // }
+    FREE(bdist)
+    FREE(dist)
+    FREE(part)
 }
 
 int main(int argc, char *argv[]) {
